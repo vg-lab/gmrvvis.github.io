2ccf3a0546ffd3fc949e7890d83672e0890b9fbf
Pablo Toharia <pablo.toharia@upm.es>
2022-02-04 11:19:51 +0100

    Buildbot updated doc for ReTo

--- doc/ReTo/ProjectInfo.cmake
+++ doc/ReTo/ProjectInfo.cmake
@@ -1,6 +1,6 @@
 set(RETO_MATURITY "EP")
 set(RETO_DESCRIPTION "")
-set(RETO_GIT_ROOT_URL "https://gitlab-ci-token:pkRtpqXa4mssdYYgsC8x@gitlab.vg-lab.es/nsviz/ReTo.git")
+set(RETO_GIT_ROOT_URL "https://gitlab-ci-token:ysBx3Ff5mdRzg2h68jd_@gitlab.vg-lab.es/nsviz/ReTo.git")
 set(RETO_PACKAGE_URL "")
 set(RETO_CI_URL "")
 set(RETO_CI_PNG "")
